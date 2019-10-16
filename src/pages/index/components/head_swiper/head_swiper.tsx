e63b103757fae8b491190061fa5efe2a683fd45c
BLANKMYMIND <zhangshunyucn@qq.com>
2019-10-16 21:58:45 +0800

    添加首页轮播图

---
+++ src/pages/index/components/head_swiper/head_swiper.tsx
@@ -0,0 +1,38 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Image, Swiper, SwiperItem} from '@tarojs/components'
+import samplePic0 from '../../../../static/images/3997/39974737/v2_pyzne4.jpg'
+import samplePic1 from '../../../../static/images/3997/39974739/v2_pyznei.jpg'
+import samplePic2 from '../../../../static/images/3997/39974774/v2_pyzngx.jpg'
+import './head_swiper.scss'
+
+
+export default class Headswiper extends Component {
+  
+  samplePic = [
+      samplePic0,
+      samplePic1,
+      samplePic2,
+  ]  
+
+  render () {
+    return (
+      <Swiper
+        className='head'
+        autoplay={true}
+        interval={3000}
+        circular={true} >
+          {
+            this.samplePic.map( (pic) => 
+              <SwiperItem>
+                <View>
+                  <Image src={pic} mode='aspectFill' className='swiperimg'></Image>
+                </View>
+              </SwiperItem>
+            )
+          }
+        
+
+      </Swiper>
+    )
+  }
+}
