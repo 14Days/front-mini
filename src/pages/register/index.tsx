23ff7b304217bb4d79f92aa79e98f82085880b46
qkuns <qkuns@qq.com>
2019-10-18 17:48:38 +0800

    fix: page:[login, register]

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,83 +1,142 @@
-import Taro, { Component } from '@tarojs/taro';
-import { View, Text, Input, Button } from '@tarojs/components';
-import { RegisterProp, RegisterState } from '../../interface/register';
+import Taro, {Component} from '@tarojs/taro';
+import {View, Text, Input, Button} from '@tarojs/components';
+import {RegisterProp, RegisterState} from '../../interface/register';
 import './index.scss';
+
 
 class Register extends Component<RegisterProp, RegisterState> {
   constructor(props: RegisterProp) {
     super(props);
 
     this.state = {
-      phonenumber: '',
+      phoneNumber: '',
       auth: 0,
       username: '',
-      firstpassword: '',
-      secondpassword:'',
+      firstPassword: '',
+      secondPassword: '',
       password: '',
+      code: '',
+      step: 1
     };
 
     this.onUseeChange = this.onUseeChange.bind(this);
   }
+
   config = {};
 
   onUseeChange(e: Event): void {
     console.log(e);
   }
+
+  //检查手机号, 改变下一步按钮的状态
+  checkPhoneNumber(): boolean {
+    return false;
+
+  }
+
+  //检查验证码
+  checkCode(): void {
+    //API
+
+    //发送验证码
+
+    //验证验证码是否匹配
+
+    //匹配
+    this.setState({
+      step: 2
+    })
+
+    //不匹配,重新输入
+  }
+
+
   render() {
-    return (
-      <View className='container'>
+    const StepOne: JSX.Element = (
+      <View>
         <View className='title'>
-          <Text className='titleText'>注册</Text>
+          <Text className='titleText'>请输入注册手机号</Text>
         </View>
-        <View className='linebetween' />
-        <View className='phonebox'>
+        <View className='phoneBox'>
           <Input
-            value={this.state.phonenumber}
+            value={this.state.phoneNumber}
             placeholder='手机号码'
-            onInput={() => {}}>
+            onInput={(e) => {
+              this.setState({
+                phoneNumber: e.target.value
+              })
+            }}
+            disabled={this.state.step !== 1}
+          >
+          </Input>
+          <Input
+            type='number'
+            placeholder='验证码'
+            onInput={(e) => {
+              this.setState({
+                code: e.target.value
+              })
+            }}
+            disabled={this.state.step !== 1}
+          >
           </Input>
         </View>
-        <Button className='sendButton' onClick={() => {}} disabled>下一步</Button>
-        
-        <View className='moreinfo'>
-          <View className='authbox'>
-            <Input
-              type='number'
-              placeholder='验证码'
-              onInput={() => {}}>
-            </Input>
-            <Button className='resendButton'>重发</Button>
-          </View>
-          <View className='username'>
+
+      </View>
+    );
+
+    const StepTwo: JSX.Element = (
+      <View>
+        <View className='userInfo'>
             <Input
               value={this.state.username}
               placeholder='用户名'
-              onInput={() => {}}>
+              onInput={() => {
+              }}>
             </Input>
-          </View>
-          <View className = 'firstPwd'>
             <Input
-              value={this.state.firstpassword}
+              value={this.state.firstPassword}
               placeholder='密码'
               password={true}
+              onInput={(e) => {
+                this.setState({
+                  firstPassword: e.target.value
+                })
+              }}
             ></Input>
-          </View>
-          <View className = 'firstPwd'>
             <Input
-              value={this.state.secondpassword}
+              value={this.state.secondPassword}
               placeholder='再次输入密码'
               password={true}
+              onInput={(e) => {
+                this.setState({
+                  secondPassword: e.target.value
+                })
+              }}
             ></Input>
-          </View>
-          <Button>确认</Button>
         </View>
-
+        <Button>确认</Button>
         <View>
           <Text className='tipWord'></Text>
         </View>
       </View>
     );
+
+    const nextStepBtn = (
+      <Button
+        className='sendButton'
+        onClick={this.checkCode}
+      >下一步</Button>
+    );
+    return (
+      <View className='container'>
+        {StepOne}
+        {this.state.step === 1 ? nextStepBtn : null}
+        {this.state.step === 2 ? StepTwo : null}
+      </View>
+    );
   }
+
 }
 
 export default Register;
