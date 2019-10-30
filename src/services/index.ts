742e5f49df6637b3c7e266ee9797e78533092655
BLANKMYMIND <zhangshunyucn@qq.com>
2019-10-30 21:29:56 +0800

    [style] api修整&添加注释

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,28 +1,34 @@
 import request from '../utils/request';
 import { getNoticeURL, getStatisticDataURL, getCycleImginURL, getUnknownURL, commitUnknownURL, getTagURL, getMarkImgURL } from '../utils/url';
 
+//获取轮播图
 export async function fetchCycle() {
   return await request.get(getCycleImginURL);
 }
 
+//获取统计数据
 export async function fetchCount() {
   return await request.get(getStatisticDataURL);
 }
 
+//获取公告
 export async function fetchNotice() {
   return await request.get(getNoticeURL);
 }
 
+//获取被搁置图片
 export async function fetchShelve() {
   return await request.get(getUnknownURL);
 }
 
+//请求图片放入搁置
 export async function passShelve(imgID) {
   return await request.post(commitUnknownURL, {
     img_id: imgID,
   })
 }
 
+//上传打标
 export async function submitLabels(id, tags) {
   return await request.post(getTagURL, {
     img_id: id,
@@ -30,6 +36,7 @@ export async function submitLabels(id, tags) {
   })
 }
 
+//请求普通图片
 export async function fetchImg() {
   return await request.get(getMarkImgURL, {
     num: 1
--- src/services/work.ts
+++ src/services/work.ts
@@ -1,37 +1,23 @@
-import Taro from '@tarojs/taro';
 import request from '../utils/request';
 import { getMarkImgURL, getTagURL, commitUnknownURL } from '../utils/url';
 
 
 export async function fetchImg() {
-  const token = Taro.getStorageSync('token')
-  console.log(token)
-  return await request.get<string>(getMarkImgURL, {
+  return await request.get(getMarkImgURL, {
     num: 1,
-    t: Math.random() * 100
-  }, {
-    token: token
   });
 }
   
 export async function deliverLabels(id: number, tags: Array<number>, type: number) {
-  const token = Taro.getStorageSync('token')
-  console.log(token)
-  return await request.post<string>(getTagURL, {
+  return await request.post(getTagURL, {
     img_id: id,
     tag: tags,
     type: type
-  }, {
-    token: token
   });
 }
 
 export async function shelveImg(imgID: number) {
-  const token = Taro.getStorageSync('token')
-  console.log(token)
-  return await request.post<string>(commitUnknownURL, {
+  return await request.post(commitUnknownURL, {
     img_id: imgID
-  }, {
-    token: token
   });
 }
